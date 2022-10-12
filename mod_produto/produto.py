c38f5d50a4f50e28134da086e4059ea90d145798
camillykahl <camillymescke@gmail.com>
2022-10-12 20:26:08 -0300

    commit do projeto

--- mod_produto/produto.py
+++ mod_produto/produto.py
@@ -2,6 +2,9 @@ from flask import Blueprint, render_template
 bp_produto = Blueprint('produto', __name__, url_prefix="/produto", template_folder='templates')
 
 ''' rotas dos formulários '''
-@bp_produto.route('/')
+@bp_produto.route('/', methods=['GET', 'POST'])
 def formListaProduto():
-    return render_template('formListaProduto.html'), 200
+    return render_template('formListaProduto.html')
+@bp_produto.route('/form-produto/', methods=['GET', 'POST'])
+def formProduto():
+    return render_template('formProduto.html')
