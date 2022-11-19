c8273f970d274e8d3b253adf785effdce4115c4a
camillykahl <camillymescke@gmail.com>
2022-11-18 22:51:35 -0300

    alteração do dia 18

--- mod_produto/produto.py
+++ mod_produto/produto.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, jsonify
 import requests
 import base64
 bp_produto = Blueprint('produto', __name__, url_prefix="/produto", template_folder='templates')
@@ -14,13 +14,30 @@ def formListaProduto():
         response = requests.get(urlApiProdutos, headers=headers)
         result = response.json()
         if(response.status_code != 200):
-            raise Exception(result)
-        return render_template('formListaProduto.html', result=result)
+            raise Exception(result[0])
+        return render_template('formListaProduto.html', result=result[0])
     except Exception as e:
         return render_template('formListaProduto.html', erro = e)
+
 @bp_produto.route('/form-produto/', methods=['GET', 'POST'])
 def formProduto():
     return render_template('formProduto.html')
+
+@bp_produto.route("/form-edit-produto", methods=['POST'])
+def formEditProduto():
+	try:
+		# ID enviado via FORM
+		id_produto = request.form['id']
+		# executa o verbo GET da API buscando somente o funcionário selecionado,
+		# obtendo o JSON do retorno
+		response = requests.get(urlApiProdutos + id_produto, headers=headers)
+		result = response.json()
+		if (response.status_code != 200 or result[1] != 200):
+			raise Exception(result[0])
+		# renderiza o form passando os dados retornados
+		return render_template('formProduto.html', result=result[0])
+	except Exception as e:
+		return render_template('formListaProduto.html', msgErro=e.args[0])
 
 
 @bp_produto.route('/insert', methods=['POST'])
@@ -30,16 +47,59 @@ def insert():
         id_produto = request.form['id']
         nome = request.form['nome']
         descricao = request.form['descricao']
+       
+
         #foto = request.form['foto']
-        valor_unitario = request.form['valor']
+        valor_unitario = request.form['valor_unitario']
         # converte a foto em base64
         foto = "data:" + request.files['foto'].content_type + ";base64," + str(base64.b64encode(request.files['foto'].read()), "utf-8")
-
         # monta o JSON para envio a API
-        payload = {'id_produto': id_produto, 'nome': nome, 'descricao': descricao, 'valor_unitario': valor_unitario, 'foto': foto}
+        payload = {'id_produto': id_produto, 'nome': nome,'descricao': descricao, 'foto': foto, 'valor_unitario': valor_unitario}
+        
         # executa o verbo POST da API e armazena seu retorno
         response = requests.post(urlApiProdutos, headers=headers, json=payload)
         result = response.json()
-        return redirect( url_for('produto.formListaProduto', msg=result) )
+
+        if (response.status_code != 200 or result[1] != 200):
+            raise Exception(result[0])
+        return jsonify(erro=False, msg=result[0])
     except Exception as e:
-        return render_template('formListaProduto.html', msgErro=e)
+        return jsonify(erro=True, msgErro=e.args[0])
+
+@bp_produto.route('/edit', methods=['POST'])
+def edit():
+	try:
+		# dados enviados via FORM
+		id_produto = request.form['id']
+		nome = request.form['nome']
+		descricao = request.form['descricao']
+		valor_unitario = request.form['valor_unitario']
+		# foto = request.files['foto']
+		# converte em base64
+		foto = "data:" + request.files['foto'].content_type + ";base64," + str(base64.b64encode(request.files['foto'].read()), "utf-8")
+		# monta o JSON para envio a API
+		payload = {'id_produto': id_produto, 'nome': nome,'descricao': descricao, 'foto': foto, 'valor_unitario': valor_unitario}
+
+		# executa o verbo PUT da API e armazena seu retorno
+		response = requests.put(urlApiProdutos + id_produto, headers=headers, json=payload)
+		result = response.json()
+		if (response.status_code != 200 or result[1] != 201):
+			raise Exception(result[0])
+		return jsonify(erro=False, msg=result[0])
+	except Exception as e:
+		return jsonify(erro=True, msgErro=e.args[0])
+
+
+@bp_produto.route('/delete', methods=['POST'])
+def delete():
+	try:
+		# dados enviados via FORM
+		id_produto = request.form['id_produto']
+		# executa o verbo DELETE da API e armazena seu retorno
+		response = requests.delete(urlApiProdutos + id_produto, headers=headers)
+		result = response.json()
+		if (response.status_code != 200 or result[1] != 201):
+			raise Exception(result[0])
+		return jsonify(erro=False, msg=result[0])
+	except Exception as e:
+		return jsonify(erro=True, msgErro=e.args[0])
